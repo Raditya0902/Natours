d95330bf44e8c180d83cfd217ea2be1e15139a16
Raditya0902 <aditya.rallapalli0902@gmail.com>
2023-07-20 17:13:01 +0530

    Added signup functionality

--- public/js/logInAndOut.js
+++ public/js/logInAndOut.js
@@ -1,15 +1,3 @@
-const hideAlert = () => {
-  const el = document.querySelector(".alert");
-  if (el) el.parentElement.removeChild(el);
-};
-
-const showAlert = (type, msg) => {
-  hideAlert();
-  const markup = `<div class="alert alert--${type}">${msg}</div>`;
-  document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
-  window.setTimeout(hideAlert, 5000);
-};
-
 const login = async (email, password) => {
   try {
     const res = await axios({
---
+++ public/js/signup.js
@@ -0,0 +1,48 @@
+const hideAlert = () => {
+  const el = document.querySelector(".alert");
+  if (el) el.parentElement.removeChild(el);
+};
+
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
+  window.setTimeout(hideAlert, 5000);
+};
+
+const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "/api/v1/users/signup",
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm,
+      },
+    });
+
+    if (res.data.status === "success") {
+      showAlert("success", "Signed in successfully!");
+      window.setTimeout(() => {
+        location.assign("/");
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert("error", err);
+  }
+};
+
+const signUpForm = document.querySelector(".form--signup");
+if (signUpForm) {
+  signUpForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("passwordConfirm").value;
+    //console.log(email, password);
+    signup(name, email, password, passwordConfirm);
+  });
+}
