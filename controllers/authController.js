9655afc16986b9828299655494c6efa0329038f8
Raditya0902 <aditya.rallapalli0902@gmail.com>
2023-07-20 19:44:22 +0530

    Changed authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,7 @@ const createToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production" || true,
   });
   user.password = undefined;
   res.status(statusCode).json({
