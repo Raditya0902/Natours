5fd30bf5b0a425d7803b1a3fb3f3ce3b62020c8d
Raditya0902 <aditya.rallapalli0902@gmail.com>
2023-07-22 00:10:34 +0530

    changed authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,16 +12,15 @@ const signToken = (id) => {
   });
 };
 
-const createToken = (user, statusCode, res) => {
+const createToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+  res.cookie("jwt", token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-  res.cookie("jwt", token, cookieOptions);
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  });
   user.password = undefined;
   res.status(statusCode).json({
     status: "success",
