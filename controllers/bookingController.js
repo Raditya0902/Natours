88c1f2ffb7d70bc1055eff7262ad13d84b33d09c
Raditya0902 <aditya.rallapalli0902@gmail.com>
2023-07-28 19:30:45 +0530

    Final Considerations 4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,6 +5,7 @@ const factory = require("./handlerFactory");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const Booking = require("../models/bookingModal");
 const AppError = require("../utils/appError");
+const { buffer } = require("micro");
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
@@ -15,26 +16,35 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // success_url: `${req.protocol}://${req.get("host")}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get("host")}/my-tours?alert=booking`,
-    cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+    success_url:
+      process.env.NODE_ENV === "development"
+        ? `${req.protocol}://localhost:8000/my-tours?alert=booking`
+        : `${req.protocol}://${req.get("host")}/my-tours?alert=booking`,
+    cancel_url:
+      process.env.NODE_ENV === "development"
+        ? `${req.protocol}://localhost:8000/tour/${tour.slug}`
+        : `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        quantity: 1,
         price_data: {
           currency: "inr",
           unit_amount: tour.price * 100,
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [
-              `${req.protocol}://${req.get("host")}/img/tours/${
-                tour.imageCover
-              }`,
-            ],
+            images:
+              process.env.NODE_ENV === "development"
+                ? [`https://www.natours.dev/img/tours/${tour.imageCover}`]
+                : [
+                    `${req.protocol}://${req.get("host")}/img/tours/${
+                      tour.imageCover
+                    }`,
+                  ],
           },
         },
+        quantity: 1,
       },
     ],
   });
@@ -72,8 +82,11 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === "checkout.session.completed")
+  if (event.type === "checkout.session.completed") {
+    console.log(event.data.object);
     createBookingCheckout(event.data.object);
+    console.log("Event was successful");
+  }
 
   res.status(200).json({ received: true });
 };
